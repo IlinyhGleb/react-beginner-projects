c8e6c97a4076bceaee20747758b2be2b36602c02
Gleb Ilinyh <gleb@ilinyh.ru>
2023-12-15 22:16:00 +0500

    добавил начальные изменения

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json()) //ответ преобразуем в json
+      .then((json) => {
+        setUsers(json.data);
+      }) //из полученного ответа берём json.data и используем в setUsers
+      .catch(err => {
+        console.log(err);
+        alert('Ошибка при получении пользователей!');
+      }); //ловим ошибку
+  }, []);
+
   return (
     <div className="App">
       <Users />
